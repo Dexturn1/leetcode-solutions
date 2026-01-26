8d235984e9cc12f5f6df80606c3d0827ade1fee0
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-01-26 16:30:51 +0530

    Time: 0 ms (100%), Space: 42.9 MB (27.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,36 @@
 class Solution {
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int []dp = new int[n];
+    //     Arrays.fill(dp,-1);
+    //     return solve(n-1,nums,dp);
+    // }
+
+    // public static int solve(int index, int []nums, int[]dp){
+    //     if(index == 0 ) return nums[index];
+    //     if(index < 0) return 0;
+
+    //     if(dp[index] != -1) return dp[index];
+
+    //     int pick = nums[index] + solve(index -2 ,nums, dp);
+    //     int notPick = 0 + solve(index -1, nums, dp);
+
+    //     return dp[index] = Math.max(pick, notPick);
+    // }
+
     public int rob(int[] nums) {
         int n = nums.length;
-        int []dp = new int[n];
-        Arrays.fill(dp,-1);
-        return solve(n-1,nums,dp);
-    }
 
-    public static int solve(int index, int []nums, int[]dp){
-        if(index == 0 ) return nums[index];
-        if(index < 0) return 0;
+        int []dp = new int [n];
+        Arrays.fill(dp, -1);
+        dp[0] = nums[0];
 
-        if(dp[index] != -1) return dp[index];
-
-        int pick = nums[index] + solve(index -2 ,nums, dp);
-        int notPick = 0 + solve(index -1, nums, dp);
-
-        return dp[index] = Math.max(pick, notPick);
+        for(int i = 1; i<n; i++){
+            int take = nums[i];
+            if(i>1)take += dp[i-2];
+            int  nonTake = 0 + dp[i-1];
+            dp[i] = Math.max(take, nonTake);
+        }
+        return dp[n-1];
     }
 }
