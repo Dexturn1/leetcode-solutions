64679470a903c3a7bb9e74123329f964ffbce176
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-01-28 18:43:25 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.83%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][]dp = new int[m][n];
+        for(int[]row :dp)Arrays.fill(row, -1);
+        return solve(m-1, n-1, dp);
+    }
+
+    int solve(int row, int col, int [][]dp){
+
+        if(dp[row][col]!= -1) return dp[row][col];
+        if(row == 0 || col == 0){
+            return 1;
+        }
+
+            return dp[row][col] = solve(row - 1, col, dp) + solve(row, col -1, dp); 
+    
+    }
+}
