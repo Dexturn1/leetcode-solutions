d5f43daa180f553177d252259b2fbeb69cb63382
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-07-28 18:09:48 +0530

    Time: 1 ms (99.86%), Space: 46.7 MB (47.13%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,74 +1,55 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
+
         char [][]board = new char[n][n];
         for(int i = 0; i<n; i++){
             Arrays.fill(board[i], '.');
         }
-        backtrack(0, board, ans);
+
+        int [] leftRow = new int[n];
+        int [] upperDigonal = new int[2* n-1];
+        int [] lowerDigonal = new int[2* n-1];
+
+        backtrack(0, board, leftRow, upperDigonal, lowerDigonal, ans);
         return ans;
+
     }
 
-    public void backtrack(int col, char[][]board, List<List<String>>ans){
+    public void backtrack(int col, char[][]board, int[]leftRow , int[]upperDigonal, int[]lowerDigonal, List<List<String>> ans){
 
         if(col == board.length){
             ans.add(construct(board));
             return;
         }
 
-
         for(int row = 0; row < board.length; row++){
-            if(isSafe(row, col , board)){
-                board[row][col] = 'Q';
-                backtrack(col+1, board, ans);
-                board[row][col] = '.';
+
+           if (leftRow[row] == 0 &&
+                lowerDigonal[row + col] == 0 &&
+                upperDigonal[board.length - 1 + col - row] == 0) {
+
+                    board[row][col] = 'Q';
+                    leftRow[row] = 1;
+                    lowerDigonal[row + col] = 1;
+                    upperDigonal[board.length - 1 + col - row] = 1;
+
+                    backtrack(col + 1, board, leftRow, upperDigonal, lowerDigonal, ans);
+
+                    board[row][col] = '.';
+                    leftRow[row] = 0;
+                    lowerDigonal[row + col] = 0;
+                    upperDigonal[board.length - 1 + col - row] = 0;
+                    
+                    }
+        }
+    }
+      public List<String> construct(char[][]board){
+
+            List<String> temp = new ArrayList<>();
+            for(char[] ch: board){
+                temp.add(new String(ch));
             }
+            return temp;
         }
-    }
-
-    public List<String> construct(char [][] board){
-        List<String> temp = new ArrayList<>();
-
-        for(char[] row: board){
-            temp.add(new String(row));
-        }
-        return temp;
-    }
-
-    private boolean isSafe(int row, int col, char[][]board){
-        int r = row;
-        int c = col;
-
-        // check for the left
-        while(c >= 0){
-            if(board[r][c] == 'Q')
-                return false;
-            c--;
-        }
-
-        //check for the upper-left dig
-        r = row;
-        c = col;
-
-        while( r >= 0 && c>= 0 ){
-
-            if(board[r][c] == 'Q')
-                return false;
-
-            c--;
-            r--;
-        }
-
-        // Check for the lower-left dig;
-        r = row;
-        c = col;
-
-        while(c>= 0 && r< board.length){
-            if(board[r][c] == 'Q')
-                return false;
-            c--;
-            r++;
-        }
-        return true;
-    }
 }
