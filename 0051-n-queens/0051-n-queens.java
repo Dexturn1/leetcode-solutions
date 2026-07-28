e5b5a9ac77ff7c8ec3a06216fa485310c1627bfe
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-07-28 18:43:43 +0530

    Time: 1 ms (99.86%), Space: 46.5 MB (82.42%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
 
         char [][]board = new char[n][n];
-        for(int i = 0; i<n; i++){
-            Arrays.fill(board[i], '.');
+
+        for(char []row: board){
+            Arrays.fill(row, '.');
         }
 
-        int [] leftRow = new int[n];
-        int [] upperDigonal = new int[2* n-1];
-        int [] lowerDigonal = new int[2* n-1];
+        int[] leftRow = new int[n];
+        int[] upperDig = new int[ 2*n -1];
+        int[] lowerDig = new int[ 2*n - 1];
 
-        backtrack(0, board, leftRow, upperDigonal, lowerDigonal, ans);
-        return ans;
+        List<List<String>> ans = new ArrayList<>();
 
+        backtrack(0, board, leftRow, upperDig, lowerDig, ans);
+        return ans;  
     }
 
-    public void backtrack(int col, char[][]board, int[]leftRow , int[]upperDigonal, int[]lowerDigonal, List<List<String>> ans){
 
+    public void backtrack(int col, char[][]board,int []leftRow ,int[] upperDig, int[] lowerDig, List<List<String>> ans){
         if(col == board.length){
             ans.add(construct(board));
             return;
@@ -25,31 +26,33 @@ class Solution {
 
         for(int row = 0; row < board.length; row++){
 
-           if (leftRow[row] == 0 &&
-                lowerDigonal[row + col] == 0 &&
-                upperDigonal[board.length - 1 + col - row] == 0) {
+            // checking if pos is safe
+            if( leftRow[row] == 0 && 
+                upperDig[row+col] == 0 &&
+                lowerDig[board.length - 1 + col - row] == 0){
 
                     board[row][col] = 'Q';
-                    leftRow[row] = 1;
-                    lowerDigonal[row + col] = 1;
-                    upperDigonal[board.length - 1 + col - row] = 1;
 
-                    backtrack(col + 1, board, leftRow, upperDigonal, lowerDigonal, ans);
+                    leftRow[row] = 1;
+                    upperDig[row+col] = 1;
+                    lowerDig[board.length - 1 + col - row] = 1; 
+
+                    backtrack(col+1, board, leftRow, upperDig, lowerDig, ans);   
 
                     board[row][col] = '.';
                     leftRow[row] = 0;
-                    lowerDigonal[row + col] = 0;
-                    upperDigonal[board.length - 1 + col - row] = 0;
-                    
-                    }
+                    upperDig[row+col] = 0;
+                    lowerDig[board.length - 1 + col - row] = 0;                
+            }
         }
     }
-      public List<String> construct(char[][]board){
 
-            List<String> temp = new ArrayList<>();
-            for(char[] ch: board){
-                temp.add(new String(ch));
-            }
-            return temp;
+    public List<String> construct(char [][]board){
+        List<String> temp = new ArrayList<>();
+        for(char[] row: board){
+            temp.add(new String(row));
         }
+        return temp;
+    }
+
 }
