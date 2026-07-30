917781646c02d5ae4bdf03e297d20a079c24bf20
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-07-31 02:54:46 +0530

    Time: 24 ms (75.31%), Space: 46.6 MB (35.31%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    int [][][]dp;
+    public int cherryPickup(int[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+        dp = new int[r][c][c];
+
+        for(int[][]p : dp){
+            for(int[] row: p){
+                Arrays.fill(row, -1);
+            }
+        }
+
+        return f(0, 0, c-1, r, c, grid);
+    }
+
+    int f(int i ,int j1, int j2, int r, int c, int[][]grid){
+
+        if(j1 < 0 || j2 < 0 || j1 >= c || j2 >=c){
+            return (int)-1e8;
+        }
+
+
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+
+        if(i == r-1){
+            if(j1 == j2) return grid[i][j1];
+            else{
+                return grid[i][j1] + grid[i][j2];
+            }
+        }
+
+        // explore all the path of both simultaneously
+        int max = (int)-1e8;
+        for(int dj1 = -1; dj1 <= +1; dj1++){
+            for(int dj2 = -1; dj2 <= +1; dj2 ++){
+                
+                int value = 0;
+
+                if(j1 == j2) 
+                     value = grid[i][j1];
+                else{
+                    value = grid[i][j1] + grid[i][j2];
+                }
+
+                value += f(i+1, j1+dj1, j2+dj2, r, c, grid);
+                max = Math.max(max,value);
+            }
+        }
+        return dp[i][j1][j2] = max;
+    }
+}
