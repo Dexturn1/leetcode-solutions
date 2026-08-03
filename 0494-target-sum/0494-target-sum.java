993c182bb37673e4c50a1620f04e69acac325df4
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-08-03 16:56:44 +0530

    Time: 626 ms (20.54%), Space: 43 MB (79.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,23 +1,26 @@
 class Solution {
+    int[][]dp;
     public int findTargetSumWays(int[] nums, int target) {
+
+        int total = target;
+        for(int x: nums)total+=x;
+
+
         return solve(nums.length-1, target, nums);
     }
 
     int solve (int index, int target, int []nums){
 
-
         if(index == -1){
-           return  target == 0 ? 1: 0;
+            return target == 0? 1:0;
         }
-        
+
 
         int pos = solve(index-1, target + nums[index], nums);
 
         int neg = solve(index-1, target - nums[index], nums);
         
 
-        return pos + neg;
-
-
+        return  pos + neg;
     }
 }
