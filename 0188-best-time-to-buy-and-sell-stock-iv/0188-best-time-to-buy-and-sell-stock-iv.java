22c4eff7607359f7d41af748501ef0a93c591f6d
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-08-05 00:59:07 +0530

    Time: 9 ms (19.38%), Space: 45.3 MB (38.27%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,36 +1,30 @@
 class Solution {
-    int [][][]dp;
     public int maxProfit(int k, int[] prices) {
-
         int n = prices.length;
 
-        dp = new int[n][2][k+1];
+        int dp[][][] = new int[n][2][k+1];
+        for(int [][]mat : dp)
+            for(int row[]: mat)Arrays.fill(row, -1);
 
-        for(int [][]x: dp)
-            for(int []y: x)Arrays.fill(y, -1);        
-
-        return solve(0, 1, k, prices);
+        return solve(0, 1, k, prices, dp);
     }
+    
+    int solve(int index, int buy, int k, int[] prices, int [][][]dp){
+        if(k == 0 || index == prices.length) return 0;
+        if(dp[index][buy][k] != -1) return dp[index][buy][k];
 
-    int solve(int index,int canBuy, int k, int []prices){
-
-        if(index == prices.length || k == 0)return 0;
-
-
-        if(dp[index][canBuy][k] != -1)
-            return dp[index][canBuy][k];
-
-        int profit;
-        if(canBuy == 1){
-            int buy = -prices[index] + solve(index+1, 0, k, prices);
-            int skip = solve(index+1, 1, k, prices);
-            profit = Math.max(buy, skip);
+        int profit; 
+        if(buy == 1){
+            int buying = -prices[index] + solve(index+1, 0, k, prices, dp);
+            int notbuy = solve(index+1, 1, k, prices, dp);
+            profit = Math.max(buying, notbuy);
         }else{
-            int sell = prices[index] + solve(index+1, 1, k-1, prices);
-            int skip = solve(index+1, 0, k, prices);
-            profit = Math.max(sell, skip);
-        }
+            int selling = prices[index] + solve(index+1, 1, k-1, prices, dp);
+            int notsell = solve(index+1, 0, k, prices,dp);
+            profit = Math.max(selling, notsell);
+        }      
 
-        return dp[index][canBuy][k] = profit;
+        return  dp[index][buy][k] = profit;
+
     }
 }
