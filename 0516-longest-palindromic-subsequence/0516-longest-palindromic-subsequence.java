30ba7561733349dce8aa212708f7d3e7cc0c3609
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-08-07 22:58:51 +0530

    Time: 24 ms (93.72%), Space: 64.1 MB (29.84%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -15,15 +15,13 @@ class Solution {
     int solve(int left, int right, String s){
         
         if(left > right)return 0;
+        if(left == right)return 1;
 
         if(dp[left][right] != -1)return dp[left][right];
 
-
         if(s.charAt(left) == s.charAt(right)){
-            if(left == right){
-                return dp[left][right] =  1 + solve(left+1, right-1, s);
-            }
             return dp[left][right] = 2 + solve(left+1, right-1, s);
+
         }else{
             return dp[left][right] = Math.max( solve(left+1, right, s) , solve(left, right-1, s));
         }
