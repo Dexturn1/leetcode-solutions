9d810212ad290ec6c1c355fca22889e1b106750e
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-08-09 01:13:10 +0530

    Time: 18 ms (76.04%), Space: 54.3 MB (52.9%) - LeetHub

--- LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.java
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,33 +1,31 @@
 class Solution {
-    int [][]dp;
     public int numDistinct(String s, String t) {
-        
+
         int n = s.length();
         int m = t.length();
-        dp = new int[n][m];
-
-        for(int []row: dp)
-            Arrays.fill(row, -1);
-
-        return solve(n-1, m-1, s, t);
-    }
-
-    int solve(int i, int j, String s, String t){
-
-        if(i == -1 && j == -1)return 1;
-        if(j == -1)return 1;
-        if(i== -1)return 0;
 
 
-        if(dp[i][j]!= -1)return dp[i][j];
+        int [][]dp = new int[n+1][m+1];
 
-        int notpick = solve(i-1, j, s, t);
-
-        int pick = 0;
-        if(s.charAt(i) == t.charAt(j)){
-            pick = solve(i-1, j-1, s, t);
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = 1;
         }
-        
-        return dp[i][j] = pick + notpick;
+
+
+        for(int i = 1; i <= n; i++){
+            for(int j =1; j<=m; j++){
+
+                int notPick = dp[i-1][j];
+                int pick = 0;
+                
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    pick = dp[i-1][j-1];
+                
+                dp[i][j] = pick+ notPick;
+            }
+        }
+
+
+        return dp[n][m];
     }
 }
