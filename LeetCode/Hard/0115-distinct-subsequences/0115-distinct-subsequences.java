eb50af206b039d42d59aa58e96c8e9e0b26a2011
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-08-09 01:00:43 +0530

    Time: 27 ms (7.01%), Space: 54.6 MB (24.47%) - LeetHub

---
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,33 @@
+class Solution {
+    int [][]dp;
+    public int numDistinct(String s, String t) {
+        
+        int n = s.length();
+        int m = t.length();
+        dp = new int[n][m];
+
+        for(int []row: dp)
+            Arrays.fill(row, -1);
+
+        return solve(n-1, m-1, s, t);
+    }
+
+    int solve(int i, int j, String s, String t){
+
+        if(i == -1 && j == -1)return 1;
+        if(j == -1)return 1;
+        if(i== -1)return 0;
+
+
+        if(dp[i][j]!= -1)return dp[i][j];
+
+        int notpick = solve(i-1, j, s, t);
+
+        int pick = 0;
+        if(s.charAt(i) == t.charAt(j)){
+            pick = solve(i-1, j-1, s, t);
+        }
+        
+        return dp[i][j] = pick + notpick;
+    }
+}
