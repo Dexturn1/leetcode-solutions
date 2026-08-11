82b6d4dfd071da886afceef843ac6669d0d9d9ae
Prabhat Kapkoti <104815270+Dexturn1@users.noreply.github.com>
2026-08-12 05:02:08 +0530

    Time: 20 ms (53.09%), Space: 54.3 MB (64.32%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,32 +1,26 @@
 class Solution {
-    int [][]dp;
     public int numDistinct(String s, String t) {
-
         int n = s.length();
         int m = t.length();
 
-        dp = new int[n][m];
+        int [][]dp = new int[n+1][m+1];
 
-        for(int []row: dp) 
-            Arrays.fill(row, -1);
 
-        return solve(n-1, m-1,  s,  t);
-    }
-
-    int solve(int i, int j, String s, String t){
-
-        if(j<0)return 1;
-        if(i<0)return 0;
+        for(int i = 0; i<=n; i++)
+            dp[i][0] = 1;
         
-        if(dp[i][j]!= -1 )return dp[i][j];
 
-        int notPick = solve(i-1, j, s, t);
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
 
-        int pick = 0;
-        if(s.charAt(i) == t.charAt(j))  
-           pick =  solve(i-1, j-1, s, t);
-
-        return dp[i][j] = pick+ notPick;
+                int notpick = dp[i-1][j];
+                int pick = 0;
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    pick = dp[i-1][j-1];
+                
+                dp[i][j] = pick+notpick;
+            }
+        }
+        return dp[n][m];
     }
-
 }
